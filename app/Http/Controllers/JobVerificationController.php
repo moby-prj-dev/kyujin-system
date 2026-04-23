ff5c24f5f9dc0559046489794b244ac754939771
moby0619-debug <moby0619@gmail.com>
2026-04-23 13:33:46 +0900

    掲載通知機能追加

--- app/Http/Controllers/JobVerificationController.php
+++ app/Http/Controllers/JobVerificationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\AdminJobNotificationMail;
 use App\Models\AuditLog;
 use App\Models\Job;
 use App\Services\SeoGeneratorService;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class JobVerificationController extends Controller
 {
@@ -34,6 +36,11 @@ class JobVerificationController extends Controller
                 'status'        => $job->status,
             ]);
         });
+
+        $adminEmail = config('mail.admin_email');
+        if ($adminEmail) {
+            Mail::to($adminEmail)->send(new AdminJobNotificationMail($job));
+        }
 
         return redirect()->route('jobs.manage', ['token' => $job->token])
             ->with('verified', true);
